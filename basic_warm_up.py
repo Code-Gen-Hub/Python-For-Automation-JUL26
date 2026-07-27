423b572afcfdd7144ae93aab04021b68b7ef4fcd
CodeGen <161267485+Code-Gen-Hub@users.noreply.github.com>
2026-07-27 10:36:00 +0800

    Create basic_warm_up.py

---
+++ basic_warm_up.py
@@ -0,0 +1,12 @@
+# loop
+# condition 1: without loop
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print('hello')
+print() # add blank line
+
+# condition 2: with loop
+for greet in range(5):
+  print('hello')
